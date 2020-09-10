b22249b83ce0c6910a2e5d21c14fcc4f81e4d7ef
Adrian Townsend <adrian.townsend@cognitivesystems.com>
2020-09-10 19:56:09 -0400

    Remove files back to base package

--- workers/mns_py/src/mns_py.py
+++ workers/mns_py/src/mns_py.py
@@ -3,25 +3,24 @@ import mzbench
 import json
 import paho.mqtt.client as mqtt
 import time
-class mns_py:
-    def initial_state():
-        pass
+def initial_state():
+    pass
 
 
-    def metrics():
-        return [
-            [
-                ('print', 'counter'),
-                ('print_2', 'counter')
-            ],
-            ('dummy', 'histogram')
-        ]
+def metrics():
+    return [
+        [
+            ('print', 'counter'),
+            ('print_2', 'counter')
+        ],
+        ('dummy', 'histogram')
+    ]
 
 
-    def my_print(msg):
-        mzbench.notify(('print', 'counter'), 1)
-        mzbench.notify(('print_2', 'counter'), 2)
+def my_print(msg):
+    mzbench.notify(('print', 'counter'), 1)
+    mzbench.notify(('print_2', 'counter'), 2)
 
-        print "{0}".format(msg)
+    print "{0}".format(msg)
 
-        mzbench.notify(('dummy', 'histogram'), random.uniform(0, 1000000000)/7)
+    mzbench.notify(('dummy', 'histogram'), random.uniform(0, 1000000000)/7)
