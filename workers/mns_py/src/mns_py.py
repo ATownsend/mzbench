d09fa557c0672ac4ecfb58276278fa3518e9f26f
Adrian Townsend <adrian.townsend@cognitivesystems.com>
2020-09-10 16:13:04 -0400

    register the class

--- workers/mns_py/src/mns_py.py
+++ workers/mns_py/src/mns_py.py
@@ -3,25 +3,25 @@ import mzbench
 import json
 import paho.mqtt.client as mqtt
 import time
-
-def initial_state():
-    pass
-
-
-def metrics():
-    return [
-        [
-            ('print', 'counter'),
-            ('print_2', 'counter')
-        ],
-        ('dummy', 'histogram')
-    ]
+class mns_py:
+    def initial_state():
+        pass
 
 
-def my_print(msg):
-    mzbench.notify(('print', 'counter'), 1)
-    mzbench.notify(('print_2', 'counter'), 2)
+    def metrics():
+        return [
+            [
+                ('print', 'counter'),
+                ('print_2', 'counter')
+            ],
+            ('dummy', 'histogram')
+        ]
 
-    print "{0}".format(msg)
 
-    mzbench.notify(('dummy', 'histogram'), random.uniform(0, 1000000000)/7)
+    def my_print(msg):
+        mzbench.notify(('print', 'counter'), 1)
+        mzbench.notify(('print_2', 'counter'), 2)
+
+        print "{0}".format(msg)
+
+        mzbench.notify(('dummy', 'histogram'), random.uniform(0, 1000000000)/7)
