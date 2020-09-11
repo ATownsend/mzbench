6bdb3bcd64654febf1a86597883d4722b8cc095b
Adrian Townsend <adrian.townsend@cognitivesystems.com>
2020-09-10 22:06:50 -0400

    print all available variables

--- workers/mns_py/src/mns_py.py
+++ workers/mns_py/src/mns_py.py
@@ -2,6 +2,7 @@ import random
 import mzbench
 import json
 import paho.mqtt.client as mqtt
+import os
 
 def initial_state():
     pass
@@ -22,6 +23,9 @@ def my_print(msg):
     mzbench.notify(('print_2', 'counter'), 2)
 
     print(msg)
+    print(os.environ)
+    print(globals())
+    print(locals())
     print(worker_id)
 
     mzbench.notify(('dummy', 'histogram'), random.uniform(0, 1000000000)/7)
