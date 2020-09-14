6489b35a5c3c895dfc5d6c32696853389a7b8543
Adrian Townsend <adrian.townsend@cognitivesystems.com>
2020-09-14 11:11:53 -0400

    detailed printout of all available variables

--- workers/mns_py/src/mns_py.py
+++ workers/mns_py/src/mns_py.py
@@ -22,10 +22,15 @@ def my_print(msg):
     mzbench.notify(('print', 'counter'), 1)
     mzbench.notify(('print_2', 'counter'), 2)
 
+    print("Booya1")
     print(msg)
+    print("Booya2")
     print(os.environ)
+    print("Booya3")
     print(globals())
+    print("Booya4")
     print(locals())
+    print("Booya5")
     print(dir())
 
     mzbench.notify(('dummy', 'histogram'), random.uniform(0, 1000000000)/7)
