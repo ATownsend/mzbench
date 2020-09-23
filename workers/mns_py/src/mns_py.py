bd649a91703d88a8dfea9e1c3082c8a03677c8a6
Adrian Townsend <adrian.townsend@cognitivesystems.com>
2020-09-23 15:15:06 -0400

    attempt to get a random mac address

--- workers/mns_py/src/mns_py.py
+++ workers/mns_py/src/mns_py.py
@@ -6,7 +6,8 @@ import paho.mqtt.client as mqtt
 import os
 import socket
 import mns.core_network_mock
-
+import mns.mac_address
+import random
 
 def initial_state():
     pass
@@ -26,6 +27,7 @@ def my_print(msg):
     mzbench.notify(('print', 'counter'), 1)
     mzbench.notify(('print_2', 'counter'), 2)
 
+    mac=mac_address.address(random.randint(0, 999999999))
     print("Booya0")
     #print(mzbench.get_worker_id())
     #print("Booya1")
