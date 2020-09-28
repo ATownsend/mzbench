e43d017b14c9c8b0101b25fb7e681607d5ce0ae8
Adrian Townsend <adrian.townsend@cognitivesystems.com>
2020-09-28 11:26:16 -0400

    rename job and fix import

--- workers/mns_py/src/mns_py.py
+++ workers/mns_py/src/mns_py.py
@@ -5,7 +5,7 @@ import time
 import paho.mqtt.client as mqtt
 import os
 import socket
-import mns.core_network_mock
+from mns.core_network_mock import core_network_mock
 from mns.mac_address import mac_address
 import random
 
@@ -23,16 +23,18 @@ def metrics():
     ]
 
 
-def my_print(server):
+def run_baseline(server):
     mzbench.notify(('print', 'counter'), 1)
     mzbench.notify(('print_2', 'counter'), 2)
 
     mac=mac_address(random.randint(0, 999999999)*256)
     time_stamp = time.time() - 31,536,000
     network = core_network_mock( mac=mac, time=time_stamp, server=server, ssl=True)
+    network.core_populate_network(time_stamp, interval, count_per_report)
     print("Booya0")
     print(random_number)
     print(mac.address())
+    #network.core_run_mqtt_status(time_stamp, interval, count_per_report)
     #print(mzbench.get_worker_id())
     #print("Booya1")
     #print(msg)
