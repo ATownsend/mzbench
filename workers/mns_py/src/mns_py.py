4402a80e55a4bf85352ba06a65fea60d6ffb4333
Adrian Townsend <adrian.townsend@cognitivesystems.com>
2020-10-08 10:24:58 -0400

    removed broken debug statement

--- workers/mns_py/src/mns_py.py
+++ workers/mns_py/src/mns_py.py
@@ -70,7 +70,6 @@ def run_registration(server):
         for status in registration["results"]:
             if status == 200 :
                 mzbench.notify(('HTTP_Success', 'counter'), 1)
-                print
             elif status == 201 :
                 mzbench.notify(('HTTP_Retry', 'counter'), 1)
                 tryagain = True
